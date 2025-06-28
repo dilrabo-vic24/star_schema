bfa05e89771784636cd13e18d6c95484767f6f7b
dilrabo-vic24 <dilrabousubjonova0@gmail.com>
2025-06-28 18:03:48 +0500

    add star schema diagram

--- src/create_dimensions.py
+++ src/create_dimensions.py
@@ -7,24 +7,24 @@ def create_dimension_tables(df_raw):
 
     #1.dim subject
     dim_subject = df_raw[['subject']].drop_duplicates().reset_index(drop = True)
-    dim_subject['subject_id'] = dim_subject.index + 1
+    dim_subject['subject_id'] = dim_user.apply(lambda _: str(uuid.uuid4()), axis=1)
     dim_subject = dim_subject[['subject', 'subject_id']]
 
     #2. dim_comm_type
     dim_comm_type = df_raw[['comm_type']].drop_duplicates().reset_index(drop = True)
-    dim_comm_type['comm_type_id'] = dim_comm_type.index + 1
+    dim_comm_type['comm_type_id'] = dim_user.apply(lambda _: str(uuid.uuid4()), axis=1)
     dim_comm_type = dim_comm_type[['comm_type_id', 'comm_type']]
 
     #3.dim_audio
     unique_audio = utils.extract_unique_from_raw_content(df_raw, "audio_url")
     dim_audio = pd.DataFrame(unique_audio, columns = ["raw_audio_url"])
-    dim_audio["audio_id"] = dim_audio.index + 1
+    dim_audio["audio_id"] = dim_user.apply(lambda _: str(uuid.uuid4()), axis=1)
     dim_audio = dim_audio[["audio_id", "raw_audio_url"]]
 
     #4.dim_video
     unique_video = utils.extract_unique_from_raw_content(df_raw, "video_url")
     dim_video = pd.DataFrame(unique_video, columns = ["raw_video_url"])
-    dim_video['video_id'] = dim_video.index + 1
+    dim_video['video_id'] = dim_user.apply(lambda _: str(uuid.uuid4()), axis=1)
     dim_video = dim_video[['video_id', 'raw_video_url']]
 
 
@@ -51,13 +51,13 @@ def create_dimension_tables(df_raw):
     #6.dim_transcript
     unique_transcript = utils.extract_unique_from_raw_content(df_raw, "transcript_url")
     dim_transcript = pd.DataFrame(unique_transcript, columns = ["raw_transcript_url"])
-    dim_transcript['transcript_id'] = dim_transcript.index + 1
+    dim_transcript['transcript_id'] = dim_user.apply(lambda _: str(uuid.uuid4()), axis=1)
     dim_transcript = dim_transcript[['transcript_id', 'raw_transcript_url']]
 
     #7.dim_calendar
     unique_calendar = utils.extract_unique_from_raw_content(df_raw, "calendar_id")
     dim_calendar = pd.DataFrame(unique_calendar, columns = ["raw_calendar_id"])
-    dim_calendar['calendar_id'] = dim_calendar.index + 1
+    dim_calendar['calendar_id'] = dim_user.apply(lambda _: str(uuid.uuid4()), axis=1)
     dim_calendar = dim_calendar[['raw_calendar_id', 'calendar_id']]
 
     #all dimensions
